ab3e715d935d84a950254a29b8c99032e2015f76
Forzana0 <sitesprog262@gmail.com>
2026-01-05 19:17:32 +0200

    connect db, create Entities, identity

---
+++ AutoRia/AutoRia/Domain/AppDbAutoRiaContext.cs
@@ -0,0 +1,32 @@
+﻿using AutoRia.Domain.Entities.Idenity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoRia.Domain;
+
+public class AppDbAutoRiaContext : IdentityDbContext<UserEntity, RoleEntity, int,
+    IdentityUserClaim<int>, UserRoleEntity, IdentityUserLogin<int>,
+    IdentityRoleClaim<int>, IdentityUserToken<int>>
+{
+    public AppDbAutoRiaContext(DbContextOptions<AppDbAutoRiaContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        //
+        builder.Entity<UserRoleEntity>()
+            .HasOne(ur => ur.User)
+            .WithMany(u => u.UserRoles)
+            .HasForeignKey(ur => ur.UserId);
+
+        builder.Entity<UserRoleEntity>()
+            .HasOne(ur => ur.Role)
+            .WithMany(u => u.UserRoles)
+            .HasForeignKey(ur => ur.RoleId);
+    }
+}
--- AutoRia/AutoRia/Program.cs
+++ AutoRia/AutoRia/Program.cs
@@ -1,6 +1,25 @@
+using AutoRia.Domain;
+using AutoRia.Domain.Entities.Idenity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Add services to the container.
+builder.Services.AddDbContext<AppDbAutoRiaContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<UserEntity, RoleEntity>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequireNonAlphanumeric = false;
+})
+    .AddEntityFrameworkStores<AppDbAutoRiaContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
